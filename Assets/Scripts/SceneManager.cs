72f35eaa45f9f727683b83e70a4a9b237441c387
Ahnaf Hassan <absuloutezero@gmail.com>
2026-07-12 12:24:49 -0400

    Default Music and Music Player

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class SceneManager : MonoBehaviour
 {
@@ -14,6 +15,10 @@ public class SceneManager : MonoBehaviour
     [SerializeField] Image playerBackground;
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource sfxSource;
+    [SerializeField] private GameObject musicPlayer;
+    [SerializeField] private TMP_Dropdown nowPlaying;
+    private Dictionary<string, TMP_Dropdown.OptionData> musicList = new Dictionary<string, TMP_Dropdown.OptionData>();
+
 
 
     private void Awake()
@@ -49,6 +54,46 @@ public class SceneManager : MonoBehaviour
         musicSource.Stop();
         musicSource.clip = clip;
         musicSource.Play();
+        musicPlayer.gameObject.SetActive(true);
+        nowPlaying.value = nowPlaying.options.IndexOf(musicList[fileName]);
+
+    }
+
+    public async void SetMusic(int index)
+    {
+        string fileName = nowPlaying.options[index].text;
+        AudioClip clip = await MusicManager.Instance.LoadMusic(fileName);
+        if (clip == null)
+            return;
+        musicSource.Stop();
+        musicSource.clip = clip;
+        musicSource.Play();
+        musicPlayer.gameObject.SetActive(true);
+
+    }
+
+    public void AddOptionData(string fileName) 
+    { 
+    
+        TMP_Dropdown.OptionData newOption = new TMP_Dropdown.OptionData();
+        newOption.text = fileName;
+        musicList[fileName] = newOption;
+        nowPlaying.options.Add(newOption);       
+
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+        musicPlayer.gameObject.SetActive(false);
+    }
+
+    public void PauseMusic()
+    {
+        if(musicSource.isPlaying)
+            musicSource.Pause();
+        else
+            musicSource.Play();
     }
 
     public async void PlaySFX(string fileName)
